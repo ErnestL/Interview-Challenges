f0e96a744361fca67755eb29d53ca9887d63df0a
Ernest Lotter <ernest.lotter@mixtelematics.com>
2022-02-24 13:34:38 +0200

    Update solution.c

--- Vehicle-Positions/solution.c
+++ Vehicle-Positions/solution.c
@@ -1,6 +1,6 @@
 /* 
  * Use your preferred naming convention
  * Add comments to help others to understand your code and to document assumptions
- * You are welcome to add additional files to the your solution if required
+ * You are welcome to add additional files if required
  * /
    
