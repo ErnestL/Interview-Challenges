0c7a6089a6fbced79054e8ef03c54701510a4ed5
bruce zeelie <brucez@gmail.co.za>
2022-04-02 15:14:00 +0200

    add supplied reference coordinates

--- Vehicle-Positions/solution.c
+++ Vehicle-Positions/solution.c
@@ -1,6 +1,15 @@
 #include "solution.h"
-
+#include <float.h>
+#include <math.h>
 struct timespec t0, t1;
+
+// reference coordinates supplied as per challenge.
+static struct vehicle_ref_coords ref_coords[] = {
+    {34.544909F, -102.100843F, FLT_MAX, 0}, {32.345544F, -99.123124F, FLT_MAX, 0},
+    {33.234235F, -100.214124F, FLT_MAX, 0}, {35.195739F, -95.348899F, FLT_MAX, 0},
+    {31.895839F, -97.789573F, FLT_MAX, 0},  {32.895839F, -101.789573F, FLT_MAX, 0},
+    {34.115839F, -100.225732F, FLT_MAX, 0}, {32.335839F, -99.992232F, FLT_MAX, 0},
+    {33.535339F, -94.792232F, FLT_MAX, 0},  {32.234235F, -100.222222F, FLT_MAX, 0}};
 
 int main(void)
 {
--- Vehicle-Positions/solution.h
+++ Vehicle-Positions/solution.h
@@ -1,4 +1,5 @@
 #include <errno.h>
+#include <inttypes.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -16,6 +17,15 @@
     {                                                                                                                  \
         exit(EXIT_FAILURE);                                                                                            \
     }
+
+// structure to store reference coords
+struct vehicle_ref_coords
+{
+    const float latitute_ref;
+    const float longitude_ref;
+    float distance;
+    int32_t position_id_nearest;
+};
 
 #define SOLUTION_H
 #endif
