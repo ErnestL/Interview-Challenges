a98d4ca357fa90c83fac6f2a4c7ea4eae398e7da
bruce zeelie <brucez@gmail.co.za>
2022-04-02 14:05:14 +0200

    initial commit

--- Vehicle-Positions/solution.c
+++ Vehicle-Positions/solution.c
@@ -1,6 +1,3 @@
-/* 
- * Use your preferred naming convention
- * Add comments to help others to understand your code and to document assumptions
- * You are welcome to add additional files if required
- * /
-   
+#include "solution.h"
+
+int main(void) {}
---
+++ Vehicle-Positions/solution.h
@@ -0,0 +1,3 @@
+#ifndef SOLUTION_H
+#define SOLUTION_H
+#endif
